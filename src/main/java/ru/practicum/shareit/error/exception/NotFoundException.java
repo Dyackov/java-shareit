873d124c156eebbc49eb12e6ec48fc4d1428d1a9
ВашÐSergo <dyackov.sergei@uandex.ru>
2024-09-03 13:25:48 +0300

    Добавлены документирующие комментарии ко всем классам в пакете error

--- src/main/java/ru/practicum/shareit/error/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/error/exception/NotFoundException.java
@@ -1,8 +1,23 @@
 package ru.practicum.shareit.error.exception;
 
+/**
+ * Исключение, которое выбрасывается, когда запрашиваемый ресурс не найден.
+ * <p>
+ * Это исключение наследуется от {@link RuntimeException} и используется для указания на случаи,
+ * когда запрашиваемые данные или ресурсы не существуют в системе.
+ * Примеры использования включают отсутствие пользователя, вещи или других объектов в хранилище.
+ */
 public class NotFoundException extends RuntimeException {
+
+    /**
+     * Конструктор класса {@code NotFoundException}.
+     *
+     * @param message Сообщение об ошибке, которое будет передано в конструктор суперкласса
+     *                {@link RuntimeException}. Это сообщение описывает причину возникновения исключения.
+     */
     public NotFoundException(String message) {
         super(message);
     }
+
 }
 
--- src/main/java/ru/practicum/shareit/error/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/handler/ErrorHandler.java
@@ -16,10 +16,21 @@ import ru.practicum.shareit.error.model.ErrorResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Класс ErrorHandler предназначен для обработки исключений, возникающих в приложении.
+ * <p>
+ * Этот класс перехватывает исключения, выбрасываемые в контроллерах, и возвращает соответствующие HTTP-статусы и сообщения об ошибках.
+ */
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
 
+    /**
+     * Обрабатывает исключение NotFoundException, выбрасываемое при попытке найти несуществующий ресурс.
+     *
+     * @param exception исключение NotFoundException.
+     * @return объект ErrorResponse с сообщением об ошибке и статусом HTTP 404 (NOT FOUND).
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse notFound(final NotFoundException exception) {
@@ -27,6 +38,12 @@ public class ErrorHandler {
         return new ErrorResponse("Не найдено", exception.getMessage());
     }
 
+    /**
+     * Обрабатывает исключение ConflictException, выбрасываемое при возникновении конфликта данных (например, при попытке создать пользователя с уже существующим email).
+     *
+     * @param exception исключение ConflictException.
+     * @return объект ErrorResponse с сообщением об ошибке и статусом HTTP 409 (CONFLICT).
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse conflict(final ConflictException exception) {
@@ -34,6 +51,12 @@ public class ErrorHandler {
         return new ErrorResponse("Ошибка.", exception.getMessage());
     }
 
+    /**
+     * Обрабатывает исключение HttpMessageNotReadableException, возникающее при неправильном формате запроса (например, при неверном формате JSON).
+     *
+     * @param exception исключение HttpMessageNotReadableException.
+     * @return объект ErrorResponse с сообщением об ошибке и статусом HTTP 400 (BAD REQUEST).
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse validate(final HttpMessageNotReadableException exception) {
@@ -41,6 +64,13 @@ public class ErrorHandler {
         return new ErrorResponse("Ошибка.", exception.getMessage());
     }
 
+    /**
+     * Обрабатывает исключение MethodArgumentNotValidException, выбрасываемое при нарушении валидации входных данных.
+     * Собирает все ошибки валидации и возвращает их в виде строки.
+     *
+     * @param exception исключение MethodArgumentNotValidException.
+     * @return объект ErrorResponse с сообщением о валидационной ошибке и статусом HTTP 400 (BAD REQUEST).
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final MethodArgumentNotValidException exception) {
@@ -52,6 +82,12 @@ public class ErrorHandler {
         return new ErrorResponse("Ошибка валидации.", errors.toString());
     }
 
+    /**
+     * Обрабатывает исключение MissingRequestHeaderException, возникающее при отсутствии необходимого заголовка в запросе.
+     *
+     * @param exception исключение MissingRequestHeaderException.
+     * @return объект ErrorResponse с сообщением об ошибке и статусом HTTP 400 (BAD REQUEST).
+     */
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse validateRequest(final MissingRequestHeaderException exception) {
